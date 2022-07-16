a491cc5e47f713f3e99e59f0da16372a805a2c87
dev <dev@developer-001>
2022-07-17 02:29:40 +0700

    added sh script for jenkins homework

---
+++ vlad_job/firstGit.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Проверяем что директория ./auto_dir существует ..."
+if ! [ -d ./auto_dir ]; then
+echo "Создание директории ./auto_dir ..." 
+mkdir ./auto_dir
+else
+echo "Директория ./auto_dir существует..."
+fi
+
+echo "Генерация имени файла..."
+filename="auto_file-$(date +"%a%d%b%s").txt"
+
+echo "Переход в директорию ./auto_dir ... "
+cd ./auto_dir
+
+echo "Создание файла с именем $filename в директории ./auto_dir ... "
+touch $filename
+
+echo "Запись данных в файл ... "
+
+echo "Текущая директория:" >> $filename
+pwd >> $filename
+echo "Информация о системе:" >> $filename
+lscpu >> $filename
